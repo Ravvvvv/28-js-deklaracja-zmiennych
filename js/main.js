db1a6f80e7b3082b82049bc7b0500e2babea61dd
rafal a <rafalandrzejczuk1@gmial.com>
2022-11-11 19:13:58 +0100

    zadanie

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/* let name = "Rafal";
+
+name = "monika"
+/* let number = 8; */
+
+/* 
+let boolean = true;
+
+let array = [1, 2, 3, 108];
+
+let objectH1 = document.querySelector('h1')
+
+objectH1.innerText = ' kurs java Script' */
+/* 
+if (number === 108) { */
+/* 
+  console.log("warunek spelniony 108");
+
+} else if (number === 110) {
+  console.log("warunek spelniony 110");
+
+} else {
+  console.log("zadaen nie zostal spelniony");
+} */
+/* let numberTxt = '9';
+
+let result = number * 2;
+if ( number !== numberTxt) {
+   */
+/*  console.log('wartosci zmienych sa nie rowne') */
+/* }
+ */
+
+/* let number = 8; 
+function multiply(num1, num2){
+  let result = num1 * num2;
+  return result;
+}
+
+console.log( multiply (number,3));
+let wynikMnozenia = multiply(108 ,2 );
+
+let myheding = document.querySelector("h1");
+myheding.textContent = ' Rafal Anrzedrzej- click';
+myheding.onclick = function(){
+alert("wlasnie klikasz h1");
+
+}
+console.log (myheding); */
+
+
+/* let firstname = "Rafal";
+let lastname = "Andrzejczuk";
+let result = (firstname , lastname)
+return result
+console.log (result);
+CZY MOZNA TAK ZROBIC??
+ */
+
+let firstname = "Rafal";
+let lastname = "Andrzejczuk";
+let result = (firstname , lastname)
+
+console.log (firstname,lastname);
